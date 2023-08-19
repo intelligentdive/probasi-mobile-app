20e175a04e3ba3d23be685fadc3e6c3968111b4c
MD SALAUDDIN <salauddincs360@gmail.com>
2023-08-19 16:15:42 +0600

    Fresh Project

---
+++ android/app/src/main/kotlin/intelligentdive/com/probashi/MainActivity.kt
@@ -0,0 +1,6 @@
+package intelligentdive.com.probashi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:probashi/views/splash_screen/splashScreen.dart';
+void main(){
+  runApp(GetMaterialApp(home: SplashScreen(),));
+}
